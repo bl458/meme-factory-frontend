9b12aa603052afc86c4dd77db7d65c6c7cf4eb59
Byungchan Lim <bl458@cornell.edu>
2020-09-28 20:07:20 +0900

    feat: Set up placeholders for Home

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { ThemeProvider } from "@material-ui/core";
 
+import Home from "./Home";
+
 import theme from "../style/theme";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">Hi</div>
+      <Home />
     </ThemeProvider>
   );
 };
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Home = () => (
+  <>
+    <Navbar />
+    <div></div>
+  </>
+);
+
+export default Home;
